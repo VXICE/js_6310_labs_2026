4502af4227e37cb97f1e89facc1d475e2b5522dd
VXICE <mr.timber1827max@mail.ru>
2026-09-18 19:17:10 +0300

    Lab 1 solution by student 18: polishing

--- lab1-js-core/solutions/student-18/solution.js
+++ lab1-js-core/solutions/student-18/solution.js
@@ -25,12 +25,12 @@ function simpleTask() {
 // ===== ЗАДАНИЕ 2: Функции =====
 function getReviewerNumber(number, lab) {
     // 2.1 Функция определяющая номер ревьюера для вашей группы по вашему номеру и номеру лабораторной работы
-    return (number + lab) % 30;
+    return ((number + lab - 1) % 30) + 1;
 }
 
 function getVariant(number, variants) {
     // 2.2 Функция определяющая номер варианта, исходя из количества вариантов
-    return number % variants;
+    return ((number - 1) % variants) + 1;
 }
 
 function calculate(a, b, operation) {
